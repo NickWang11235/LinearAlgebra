191fce815b86d2a455c5294d56ef7a0b198e3e46
Derick Tseng <derickboss1@gmail.com>
2019-07-08 20:47:19 -0700

    changes to Fraction class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-import java.util.SortedMap;
-
 public class Main {
     public static void main(String[] args) {
+
         //construct new matrices with 2d double array
         double[][] arrA = {{2,1,0},{0,1,-1},{-2,1,1}};
         double[][] arrB = {{1},{2},{-2}};
         //call methods on Matrix object, ex m.inverse(true)
         Matrix A = new Matrix(arrA);
         Matrix B = new Matrix(arrB);
+
+
         /*
         Supported operations:
             -addition
@@ -20,6 +21,8 @@ public class Main {
             -different basic row operations
         Answers are given in reduced fraction form.
          */
+
+        System.out.println(new Fraction((double)333/999));
 
         System.out.println(A.augment(B).reduce(Matrix.Reduction.RREF,true));
     }
